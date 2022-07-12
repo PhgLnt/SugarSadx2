c81740c73eccaf10ef52218f48920d17cc14c0c2
RouiePLNT <18521272@gm.uit.edu.vn>
2022-07-12 17:50:03 +0700

    .

--- cache/smarty/templates_c/%%AD^AD6^AD650F4B%%customProductInfo.tpl.php
+++ cache/smarty/templates_c/%%AD^AD6^AD650F4B%%customProductInfo.tpl.php
@@ -1,27 +1,29 @@
-<?php /* Smarty version 2.6.11, created on 2022-07-12 09:11:30
+<?php /* Smarty version 2.6.11, created on 2022-07-12 10:47:19
          compiled from custom/modules/P_CustomInvoices/tpls/customProductInfo.tpl */ ?>
 
-    <table>
+<link rel="stylesheet" href="custom.css">
+    <table class="center">
         <br>
         <tr>
-            <td>STT</td>
-            <td>Name</td>
-            <td>Unit Price</td>
-            <td>Quantity</td>
-            <td>Total</td>
+            <td style="text-align: center">STT</td>
+            <td style="text-align: center">Name</td>
+            <td style="text-align: center">Unit Price</td>
+            <td style="text-align: center">Quantity</td>
+            <td style="text-align: center">Total</td>
         </tr>
         <tr>
-        <?php $_from = $this->_tpl_vars['arrayResult']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
+            <?php $_from = $this->_tpl_vars['arrayResult']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
     foreach ($_from as $this->_tpl_vars['col']):
 ?>
         <tr>
             <?php $_from = $this->_tpl_vars['col']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
     foreach ($_from as $this->_tpl_vars['row']):
 ?>
-                <td><?php echo $this->_tpl_vars['row']; ?>
+                <td style="text-align: center"><?php echo $this->_tpl_vars['row']; ?>
 </td>
             <?php endforeach; endif; unset($_from); ?>
         </tr>
         <?php endforeach; endif; unset($_from); ?>
         </tr>
     </table>
+
--- custom/modules/P_CustomInvoices/tpls/customProductInfo.tpl
+++ custom/modules/P_CustomInvoices/tpls/customProductInfo.tpl
@@ -1,20 +1,22 @@
 
-    <table>
+<link rel="stylesheet" href="custom.css">
+    <table class="center">
         <br>
         <tr>
-            <td>STT</td>
-            <td>Name</td>
-            <td>Unit Price</td>
-            <td>Quantity</td>
-            <td>Total</td>
+            <td style="text-align: center">STT</td>
+            <td style="text-align: center">Name</td>
+            <td style="text-align: center">Unit Price</td>
+            <td style="text-align: center">Quantity</td>
+            <td style="text-align: center">Total</td>
         </tr>
         <tr>
-        {foreach from=$arrayResult item=col}
+            {foreach from=$arrayResult item=col}
         <tr>
             {foreach from=$col item=row}
-                <td>{$row}</td>
+                <td style="text-align: center">{$row}</td>
             {/foreach}
         </tr>
         {/foreach}
         </tr>
     </table>
+
